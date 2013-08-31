bc48195ce4989484d210b02ce8b94374813d5f88
Danilo Oliveira Lima <danilo__oliveira@hotmail.com>
2013-08-31 18:34:31 -0300

    Resgatando usuario da sessão e adicionado jQuery

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,12 +1,9 @@
 module.exports = function(app) {
 	var ChatController = {
 		index: function(req, res){
-			var resultado = {
-				email : req.params.email,
-				usuario : req.session.usuario
-			};
+			var params = {email : req.params.email};
 
-			res.render('chat/index', resultado);
+			res.render('chat/index', params);
 		}
 	};
 	return ChatController;
